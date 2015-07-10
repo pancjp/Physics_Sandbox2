48a670f883fea6e4c2ecb3d9baf34e32092a17b2
Travis C <tc95555@gmail.com>
2015-07-10 13:32:37 -0500

    beginning on Brick class

---
+++ Physics Sandbox/Brick.swift
@@ -0,0 +1,23 @@
+//
+//  Brick.swift
+//  Physics Sandbox
+//
+//  Created by travis on 7/10/15.
+//  Copyright © 2015 Cagan Hawthorne. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Brick : UIView {
+    
+    var brick = UIView()
+    
+    
+    brick = UIView(frame: CGRectMake(20, 20, 40, 20))
+    brick.backgroundColor = UIColor.blueColor()
+    
+    init(){
+        
+    }
+}
