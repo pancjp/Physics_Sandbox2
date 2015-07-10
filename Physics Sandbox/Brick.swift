5ac464badc43c0af0d5a345372f9e5b2a662bf86
Travis C <tc95555@gmail.com>
2015-07-10 13:35:45 -0500

    updating brick using pagan’s ball as a template

--- Physics Sandbox/Brick.swift
+++ Physics Sandbox/Brick.swift
@@ -11,13 +11,27 @@ import UIKit
 
 class Brick : UIView {
     
-    var brick = UIView()
+    var brickDynamicBehavior = UIDynamicItemBehavior()
     
     
-    brick = UIView(frame: CGRectMake(20, 20, 40, 20))
-    brick.backgroundColor = UIColor.blueColor()
-    
-    init(){
+    init(x: CGFloat, y: CGFloat)
+    {
+        super.init(frame: CGRectMake(x, y, 20, 40))
+        self.backgroundColor = UIColor.blueColor()
+        //self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        brickDynamicBehavior = UIDynamicItemBehavior(items: [self])
+        brickDynamicBehavior.friction = 0.1
+        brickDynamicBehavior.resistance = 1
+        brickDynamicBehavior.elasticity = 1.0
+        brickDynamicBehavior.density = 5
+        brickDynamicBehavior.allowsRotation = true
         
     }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
 }
